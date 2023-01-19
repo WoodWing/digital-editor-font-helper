db213b8d6ace77312ea3c5293ba1977c1ecc9486
tpiwoodwing <tpi@woodwing.com>
2023-01-19 09:38:55 +0100

    Switched to lib-font library which supports woff2 and added test fonts

--- script.js
+++ script.js
@@ -12,27 +12,44 @@ function init() {
  * @param {*} evt 
  */
 function handleFileSelect(evt) {
+  // Grab file frop drop event or file upload
+  const file = evt.target.files[0];
 
-  var f = evt.target.files[0]; 
-  var reader = new FileReader();
-  
-  reader.onload = (function (theFile) {
-    return function (e) {
-      const arrayBuffer = e.target.result;
+  // Use FileReader to, well, read the file
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
 
-      //Converting font to base 64            
-      var base64String = window.btoa(
-        new Uint8Array(arrayBuffer)
-          .reduce((data, byte) => data + String.fromCharCode(byte), '')
-      )
+  reader.onload = (e) => {
+    //Converting font to base 64            
+    const arrayBuffer = e.target.result;
+    var base64String = window.btoa(
+      new Uint8Array(arrayBuffer)
+        .reduce((data, byte) => data + String.fromCharCode(byte), '')
+    )
 
-      //Extract font information
-      const extension = f.name.split('.').pop().toLowerCase();
-      const font = opentype.parse(arrayBuffer);
-      console.log (font);
-      const fontFamily = font.names.fullName.en;
-      const postScriptName = font.names.postScriptName.en;
+    // Create a font object
+    const font = new Font("My Font Name");
+
+    // Pass the buffer, and the original filename
+    font.fromDataBuffer(reader.result, file.name);
+
+    font.onload = (evt) => {
+
+
+      // Map the details LibFont gathered from the font to the
+      // "font" variable
+      const font = evt.detail.font;
+
+      // From all the OpenType tables in the font, take the "name"
+      // table so we can inspect it further
+      const { name } = font.opentype.tables;
+
+      // https://docs.microsoft.com/en-us/typography/opentype/spec/name
+      const extension = file.name.split('.').pop().toLowerCase();
+      const fontFamily = name.get(4);
+      const postScriptName = name.get(6);
       const charStyleName = fontFamily.replaceAll(" ", "-").toLowerCase();
+
       let mimeType = "";
       // https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face#font_mime_types
       if (extension === "ttf") {
@@ -47,10 +64,10 @@ function handleFileSelect(evt) {
         alert("Please select a ttf, otf, woff or woff2 font")
         return;
       }
-      
+
       //Create CSS + AN
       const characterStyleCSS =
-`/*Character Style cs-${charStyleName} */
+        `/*Character Style cs-${charStyleName} */
 @font-face {
   font-family: '${postScriptName}';
   src: url('data:${mimeType};base64,${base64String}')
@@ -59,9 +76,9 @@ function handleFileSelect(evt) {
 .cs-${charStyleName} {
   font-family: '${postScriptName}'
 }`;
-                
+
       const anStyle =
-`{
+        `{
   "selector": {
     "textStyle": "cs-${charStyleName}"
   },
@@ -75,11 +92,10 @@ function handleFileSelect(evt) {
       document.getElementById('characterStyleTechNameInput').value = charStyleName;
       document.getElementById('characterStyleCSSTA').value = characterStyleCSS;
       document.getElementById('anStyleTA').value = anStyle;
-    };
-  })(f);
 
-  // Read in the font as array buffer
-  reader.readAsArrayBuffer(f);
+    }
+  }
+
 }
 
 /**
